ab868b3e2adbdd583cd8cabf1ee0f145e06327ff
masjohncook <mas.john.cook@gmail.com>
2022-12-07 11:38:21 +0800

    dev - insert the model path and joblib

--- dmicaedge/dmicaapi/apps.py
+++ dmicaedge/dmicaapi/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
+import os
+import joblib
+from django.conf import settings
 
 
 class DmicaapiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'dmicaapi'
+    MODEL_FIlE = os.path.join(settings.MODELS, "Predictions.joblib")
+    model =joblib.load(MODEL_FIlE)
+    
+    
