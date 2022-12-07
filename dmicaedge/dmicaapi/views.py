7df01a7da53202df9d4ec9e66a516d7e3cb9ed91
Hooje <a9584627155@gmail.com>
2022-12-07 17:15:35 +0800

    add vigilant function and change views.py

--- dmicaedge/dmicaapi/views.py
+++ dmicaedge/dmicaapi/views.py
@@ -5,6 +5,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.shortcuts import render
 
+import sys 
+sys.path.append("..")
+from backend.model.yolov7-pose-estimation.pose-estimate import detect
+
 # Create your views here.
 class HandPredictions(APIView):
     def post(self, request):
@@ -16,9 +20,10 @@ class HandPredictions(APIView):
         else :
             pass
         
-        prediction_model = AppConfig.model
-        prediction_result = prediction_model.predict()
-        response_data = prediction_result
+        ret = detect(data)
+        # ret will be 'None' if no one start or stop in this image 
+        # ret[0] is [x, y, z]
+        # ret[1] is 'left' or 'right' 
         return Response(response_data, status=200)
     
 class MicControl(APIView):
