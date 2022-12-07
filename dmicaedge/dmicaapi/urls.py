2d05a683548a0bdfb16d44a8b0555605ac6ff8c0
masjohncook <mas.john.cook@gmail.com>
2022-12-07 11:38:35 +0800

    dev - add urls in app

--- dmicaedge/dmicaapi/urls.py
+++ dmicaedge/dmicaapi/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import dmicaedge
 
 urlpatterns = [
-    path('weights'),
+    path('prediction/', HandPredictions.as_view(), name='HandPredictions'),
+    path('mic_control/', MicControl.as_view(), name='MicControl'),
 ]
