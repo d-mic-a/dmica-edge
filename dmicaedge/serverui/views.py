1d4c1a1df5c37e0cfe95169bf6adc4a406119a42
311581031_Justin <62976443+justinkong0623@users.noreply.github.com>
2022-12-09 19:58:53 +0800

    Update views.py
    add table

--- dmicaedge/serverui/views.py
+++ dmicaedge/serverui/views.py
@@ -1,5 +1,5 @@
 # Create your views here.
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404,render
 from serverui.models import Music
 from serverui.models import fun_raw_sql_query, fun_sql_cursor_update
 from serverui.serializers import MusicSerializer, MusicSerializerV1
@@ -8,7 +8,7 @@ from rest_framework import viewsets, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
-from rest_framework.decorators import detail_route, list_route
+from rest_framework.decorators import action
 
 
 # Create your views here.
@@ -16,9 +16,8 @@ class MusicViewSet(viewsets.ModelViewSet):
     queryset = Music.objects.all()
     serializer_class = MusicSerializer
     permission_classes = (IsAuthenticated,)
-
     # /api/music/{pk}/detail/
-    @detail_route(methods=['get'])
+    @action(detail=True,methods=['get'])
     def detail(self, request, pk=None):
         music = get_object_or_404(Music, pk=pk)
         result = {
@@ -29,13 +28,13 @@ class MusicViewSet(viewsets.ModelViewSet):
         return Response(result, status=status.HTTP_200_OK)
 
     # /api/music/all_singer/
-    @list_route(methods=['get'])
+    @action(detail=True,methods=['get'],url_path='all_singer')
     def all_singer(self, request):
         music = Music.objects.values_list('singer', flat=True).distinct()
         return Response(music, status=status.HTTP_200_OK)
 
     # /api/music/raw_sql_query/
-    @list_route(methods=['get'])
+    @action(detail=True,methods=['get'])
     def raw_sql_query(self, request):
         song = request.query_params.get('song', None)
         music = fun_raw_sql_query(song=song)
@@ -43,7 +42,7 @@ class MusicViewSet(viewsets.ModelViewSet):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     # /api/music/{pk}/sql_cursor_update/
-    @detail_route(methods=['put'])
+    @action(detail=True,methods=['put'])
     def sql_cursor_update(self, request, pk=None):
         song = request.data.get('song', None)
         if song:
@@ -51,7 +50,7 @@ class MusicViewSet(viewsets.ModelViewSet):
             return Response(music, status=status.HTTP_200_OK)
 
     # /api/music/version_api/
-    @list_route(methods=['get'])
+    @action(detail=True,methods=['get'])
     def version_api(self, request):
         music = Music.objects.all()
         if self.request.version == '1.0':
@@ -59,3 +58,8 @@ class MusicViewSet(viewsets.ModelViewSet):
         else:
             serializer = MusicSerializer(music, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    @action(detail=False,methods=['get'],url_path='musics')
+    def index(self,request):
+        musics=Music.objects.all()
+
+        return render(request, "musics/index.html",{"musics":musics})
