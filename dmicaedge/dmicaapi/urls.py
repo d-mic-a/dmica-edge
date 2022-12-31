9f1595493af4c6a835bde28c97d98082e230d95c
masjohncook <mas.john.cook@gmail.com>
2022-12-31 11:58:15 +0800

    dev

--- dmicaedge/dmicaapi/urls.py
+++ dmicaedge/dmicaapi/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from dmicaapi import views
 
 urlpatterns = [
-    path('requester_action', views.detect)
+    path('requester_action/', views.detect)
     # path('requester_action/', RequesterAction.as_view(), name='RequesterAction'),
     # path('vigilant_information/', VigilantInformation.as_view(), name='VigilantInformation'),
     # path('observant_control/', ObservantControl.as_view(), name='ObservantControl'),
