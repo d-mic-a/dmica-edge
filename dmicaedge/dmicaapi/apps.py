ffbe10fcafa1db57bba1976cd0bc56f9fc20b69b
Hooje <a9584627155@gmail.com>
2022-12-31 10:25:40 +0800

    change apps

--- dmicaedge/dmicaapi/apps.py
+++ dmicaedge/dmicaapi/apps.py
@@ -7,5 +7,5 @@ from django.conf import settings
 class DmicaapiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'dmicaapi'
-    MODEL_FIlE = os.path.join(settings.MODELS, "PoseEstimate.joblib")
-    model =joblib.load(MODEL_FIlE)
+    #MODEL_FIlE = os.path.join(settings.MODELS, "PoseEstimate.joblib")
+    #model =joblib.load(MODEL_FIlE)
