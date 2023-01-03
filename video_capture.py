b28fe09a56698c126f80eb37f7f390c04fab2031
masjohncook <mas.john.cook@gmail.com>
2023-01-03 16:15:08 +0800

    dev add video stream test

---
+++ video_capture.py
@@ -0,0 +1,25 @@
+from threading import Thread
+import gi
+from time import sleep
+
+gi.require_version('Gst', '1.0')
+from gi.repository import Gst, GLib
+
+Gst.init()
+
+main_loop = GLib.MainLoop()
+main_loop_thread = Thread(target=main_loop.run)
+main_loop_thread.start()
+
+pipeline = Gst.parse_launch("udpsrc port=5200 ! application/x-rtp, media=video, clock-rate=90000, payload=96 ! rtpjpegdepay ! jpegdec ! videoconvert ! autovideosink")
+pipeline.set_state(Gst.State.PLAYING)
+
+try:
+    while True:
+        sleep(0.1)
+except KeyboardInterrupt:
+    pass
+
+pipeline.set_state(Gst.State.NULL)
+main_loop.quit()
+main_loop_thread.join()
